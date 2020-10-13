cef909e95066a51d6cec4ef588416efdfcd68a23
Fernando Yuji Mochizuki Okano <69857686+FernandoY2203@users.noreply.github.com>
2020-10-13 03:59:08 -0300

    Add files via upload

---
+++ Atvd5/Atvd5 - Desconto/Atvd5 - Desconto/Form1.Designer.cs
@@ -0,0 +1,410 @@
+﻿namespace Atvd5___Desconto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbDescri = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.mtbSalBrut = new System.Windows.Forms.MaskedTextBox();
+            this.mtbNumFilhos = new System.Windows.Forms.MaskedTextBox();
+            this.mtbAliINSS = new System.Windows.Forms.MaskedTextBox();
+            this.mtbAliIRPF = new System.Windows.Forms.MaskedTextBox();
+            this.mtbSalFam = new System.Windows.Forms.MaskedTextBox();
+            this.mtbSalLiq = new System.Windows.Forms.MaskedTextBox();
+            this.mtbDescINSS = new System.Windows.Forms.MaskedTextBox();
+            this.mtbDescIRPF = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbSexFem = new System.Windows.Forms.RadioButton();
+            this.rbSexMasc = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbEstCivCasa = new System.Windows.Forms.RadioButton();
+            this.rbEstCivSolt = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(19, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome do Funcionario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(78, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Salario Bruto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(46, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numero de Filhos";
+            // 
+            // lbDescri
+            // 
+            this.lbDescri.AutoSize = true;
+            this.lbDescri.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbDescri.Location = new System.Drawing.Point(19, 201);
+            this.lbDescri.Name = "lbDescri";
+            this.lbDescri.Size = new System.Drawing.Size(0, 20);
+            this.lbDescri.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(70, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Aliquota INSS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(71, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Aliquota IRPF";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(67, 344);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Salario Familia";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label8.Location = new System.Drawing.Point(67, 376);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Salario Liquido";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label9.Location = new System.Drawing.Point(415, 280);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(120, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Desconto INSS";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label10.Location = new System.Drawing.Point(416, 312);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(119, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Desconto IRPF";
+            // 
+            // textName
+            // 
+            this.textName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textName.Location = new System.Drawing.Point(185, 23);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(248, 26);
+            this.textName.TabIndex = 10;
+            // 
+            // mtbSalBrut
+            // 
+            this.mtbSalBrut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.mtbSalBrut.Location = new System.Drawing.Point(185, 59);
+            this.mtbSalBrut.Mask = "999,990.99";
+            this.mtbSalBrut.Name = "mtbSalBrut";
+            this.mtbSalBrut.Size = new System.Drawing.Size(126, 26);
+            this.mtbSalBrut.TabIndex = 11;
+            // 
+            // mtbNumFilhos
+            // 
+            this.mtbNumFilhos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.mtbNumFilhos.Location = new System.Drawing.Point(185, 95);
+            this.mtbNumFilhos.Mask = "90";
+            this.mtbNumFilhos.Name = "mtbNumFilhos";
+            this.mtbNumFilhos.Size = new System.Drawing.Size(63, 26);
+            this.mtbNumFilhos.TabIndex = 12;
+            // 
+            // mtbAliINSS
+            // 
+            this.mtbAliINSS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.mtbAliINSS.Location = new System.Drawing.Point(185, 277);
+            this.mtbAliINSS.Name = "mtbAliINSS";
+            this.mtbAliINSS.Size = new System.Drawing.Size(126, 26);
+            this.mtbAliINSS.TabIndex = 13;
+            // 
+            // mtbAliIRPF
+            // 
+            this.mtbAliIRPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.mtbAliIRPF.Location = new System.Drawing.Point(185, 309);
+            this.mtbAliIRPF.Name = "mtbAliIRPF";
+            this.mtbAliIRPF.Size = new System.Drawing.Size(126, 26);
+            this.mtbAliIRPF.TabIndex = 14;
+            // 
+            // mtbSalFam
+            // 
+            this.mtbSalFam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.mtbSalFam.Location = new System.Drawing.Point(185, 341);
+            this.mtbSalFam.Name = "mtbSalFam";
+            this.mtbSalFam.Size = new System.Drawing.Size(126, 26);
+            this.mtbSalFam.TabIndex = 15;
+            // 
+            // mtbSalLiq
+            // 
+            this.mtbSalLiq.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.mtbSalLiq.Location = new System.Drawing.Point(185, 373);
+            this.mtbSalLiq.Name = "mtbSalLiq";
+            this.mtbSalLiq.Size = new System.Drawing.Size(126, 26);
+            this.mtbSalLiq.TabIndex = 16;
+            // 
+            // mtbDescINSS
+            // 
+            this.mtbDescINSS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.mtbDescINSS.Location = new System.Drawing.Point(541, 277);
+            this.mtbDescINSS.Name = "mtbDescINSS";
+            this.mtbDescINSS.Size = new System.Drawing.Size(126, 26);
+            this.mtbDescINSS.TabIndex = 17;
+            // 
+            // mtbDescIRPF
+            // 
+            this.mtbDescIRPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.mtbDescIRPF.Location = new System.Drawing.Point(541, 309);
+            this.mtbDescIRPF.Name = "mtbDescIRPF";
+            this.mtbDescIRPF.Size = new System.Drawing.Size(126, 26);
+            this.mtbDescIRPF.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(360, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 41);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Verificar Desconto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbSexFem);
+            this.groupBox1.Controls.Add(this.rbSexMasc);
+            this.groupBox1.Location = new System.Drawing.Point(454, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(104, 71);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo";
+            // 
+            // rbSexFem
+            // 
+            this.rbSexFem.AutoSize = true;
+            this.rbSexFem.Checked = true;
+            this.rbSexFem.Location = new System.Drawing.Point(19, 43);
+            this.rbSexFem.Name = "rbSexFem";
+            this.rbSexFem.Size = new System.Drawing.Size(67, 17);
+            this.rbSexFem.TabIndex = 1;
+            this.rbSexFem.TabStop = true;
+            this.rbSexFem.Text = "Feminino";
+            this.rbSexFem.UseVisualStyleBackColor = true;
+            // 
+            // rbSexMasc
+            // 
+            this.rbSexMasc.AutoSize = true;
+            this.rbSexMasc.Location = new System.Drawing.Point(19, 20);
+            this.rbSexMasc.Name = "rbSexMasc";
+            this.rbSexMasc.Size = new System.Drawing.Size(73, 17);
+            this.rbSexMasc.TabIndex = 0;
+            this.rbSexMasc.TabStop = true;
+            this.rbSexMasc.Text = "Masculino";
+            this.rbSexMasc.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbEstCivCasa);
+            this.groupBox2.Controls.Add(this.rbEstCivSolt);
+            this.groupBox2.Location = new System.Drawing.Point(564, 11);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(104, 71);
+            this.groupBox2.TabIndex = 21;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Estado Civil";
+            // 
+            // rbEstCivCasa
+            // 
+            this.rbEstCivCasa.AutoSize = true;
+            this.rbEstCivCasa.Location = new System.Drawing.Point(16, 19);
+            this.rbEstCivCasa.Name = "rbEstCivCasa";
+            this.rbEstCivCasa.Size = new System.Drawing.Size(73, 17);
+            this.rbEstCivCasa.TabIndex = 2;
+            this.rbEstCivCasa.TabStop = true;
+            this.rbEstCivCasa.Text = "Casado(a)";
+            this.rbEstCivCasa.UseVisualStyleBackColor = true;
+            // 
+            // rbEstCivSolt
+            // 
+            this.rbEstCivSolt.AutoSize = true;
+            this.rbEstCivSolt.Checked = true;
+            this.rbEstCivSolt.Location = new System.Drawing.Point(16, 42);
+            this.rbEstCivSolt.Name = "rbEstCivSolt";
+            this.rbEstCivSolt.Size = new System.Drawing.Size(72, 17);
+            this.rbEstCivSolt.TabIndex = 3;
+            this.rbEstCivSolt.TabStop = true;
+            this.rbEstCivSolt.Text = "Solteiro(a)";
+            this.rbEstCivSolt.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.Location = new System.Drawing.Point(517, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 41);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button3.Location = new System.Drawing.Point(463, 358);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 41);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 420);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mtbDescIRPF);
+            this.Controls.Add(this.mtbDescINSS);
+            this.Controls.Add(this.mtbSalLiq);
+            this.Controls.Add(this.mtbSalFam);
+            this.Controls.Add(this.mtbAliIRPF);
+            this.Controls.Add(this.mtbAliINSS);
+            this.Controls.Add(this.mtbNumFilhos);
+            this.Controls.Add(this.mtbSalBrut);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbDescri);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Desconto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbDescri;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.MaskedTextBox mtbSalBrut;
+        private System.Windows.Forms.MaskedTextBox mtbNumFilhos;
+        private System.Windows.Forms.MaskedTextBox mtbAliINSS;
+        private System.Windows.Forms.MaskedTextBox mtbAliIRPF;
+        private System.Windows.Forms.MaskedTextBox mtbSalFam;
+        private System.Windows.Forms.MaskedTextBox mtbSalLiq;
+        private System.Windows.Forms.MaskedTextBox mtbDescINSS;
+        private System.Windows.Forms.MaskedTextBox mtbDescIRPF;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbSexFem;
+        private System.Windows.Forms.RadioButton rbSexMasc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbEstCivCasa;
+        private System.Windows.Forms.RadioButton rbEstCivSolt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Atvd5/Atvd5 - Desconto/Atvd5 - Desconto/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atvd5___Desconto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textName.Clear();
+            mtbSalBrut.Clear();
+            mtbNumFilhos.Clear();
+            mtbAliINSS.Clear();
+            mtbAliIRPF.Clear();
+            mtbSalFam.Clear();
+            mtbSalLiq.Clear();
+            mtbDescINSS.Clear();
+            mtbDescIRPF.Clear();
+            lbDescri.Text = "";
+            textName.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double SalBrut;
+            int NumFilhos;
+
+            if (int.TryParse(mtbNumFilhos.Text, out NumFilhos))
+            {
+                if (double.TryParse(mtbSalBrut.Text, out SalBrut))
+                {
+                    string Nome;
+                    double descINSS1, descINSS2, descINSS3, descINSS4, Desc, descIRPF = 0, salFami = 0, SalBase, SalLiq;
+
+                    Nome = textName.Text;
+
+                    if(rbSexFem.Checked)
+                    {
+                        if(rbEstCivSolt.Checked)
+                        {
+                            lbDescri.Text = "Os Descontos do Salario da Sra." + Nome + "\n" +
+                                "que è Solteira \n" +
+                                "e tem " + NumFilhos + " Filhos(s) Sao: ";
+                        }
+                        else
+                        {
+                            lbDescri.Text = "Os Descontos do Salario da Sra." + Nome + "\n" +
+                                "que è Casada \n" +
+                                "e tem " + NumFilhos + " Filhos(s) Sao: ";
+                        }
+                    }
+                    else
+                        if(rbEstCivSolt.Checked)
+                    {
+                        lbDescri.Text = "Os Descontos do Salario do Sr." + Nome + "\n" +
+                                "que è Solteiro \n" +
+                                "e tem " + NumFilhos + " Filhos(s) Sao: ";
+                    }
+                    else
+                    {
+                        lbDescri.Text = "Os Descontos do Salario do Sr." + Nome + "\n" +
+                                "que è Casado \n" +
+                                "e tem " + NumFilhos + " Filhos(s) Sao: ";
+                    }
+
+                    if(SalBrut > 1045.00)
+                    {
+                        descINSS1 = 1045.00 * 0.075;
+
+                        if(SalBrut > 2089.61)
+                        {
+                            descINSS2 = (2089.61 - 1045.00) * 0.09;
+
+                            if(SalBrut > 3134.41)
+                            {
+                                descINSS3 = (3134.41 - 2089.61) * 0.12;
+
+                                if(SalBrut > 6101.06)
+                                {
+                                    descINSS4 = (6101.06 - 3134.41) * 0.14;
+                                    mtbAliINSS.Text = "TETO";
+                                    Desc = descINSS1 + descINSS2 + descINSS3 + descINSS4;
+                                    mtbDescINSS.Text = Desc.ToString("N2");
+                                }
+                                else
+                                {
+                                    mtbAliINSS.Text = "14,00%";
+                                    descINSS4 = (SalBrut - 3134.41) * 0.14;
+                                    Desc = descINSS1 + descINSS2 + descINSS3 + descINSS4;
+                                    mtbDescINSS.Text = Desc.ToString("N2");
+                                }
+                            }
+                            else
+                            {
+                                mtbAliINSS.Text = "12,00%";
+                                descINSS3 = (SalBrut - 2089.61) * 0.12;
+                                Desc = descINSS1 + descINSS2 + descINSS3;
+                                mtbDescINSS.Text = Desc.ToString("N2");
+                            }
+                        }
+                        else
+                        {
+                            mtbAliINSS.Text = "9,00%";
+                            descINSS2 = (SalBrut - 1045.00) * 0.09;
+                            Desc = descINSS1 + descINSS2;
+                            mtbDescINSS.Text = Desc.ToString("N2");
+
+                        }
+                    }
+                    else
+                    {
+                        mtbAliINSS.Text = "7,50%";
+                        descINSS1 = SalBrut * 0.075;
+                        Desc = descINSS1;
+                        mtbDescINSS.Text = Desc.ToString("N2");
+                    }
+
+                    SalBase = SalBrut - Desc - (NumFilhos * 189.59);
+
+                    if(SalBase > 1903.98 && SalBase <= 2826.65)
+                    {
+                        mtbAliIRPF.Text = "7,50%";
+                        descIRPF = SalBase * 0.075 - 142.80;
+                        mtbDescIRPF.Text = descIRPF.ToString("N2");
+
+                    }
+                    else if(SalBase > 2826.65 && SalBase <= 3751.05)
+                    {
+                        mtbAliIRPF.Text = "15,00%";
+                        descIRPF = SalBase * 0.15 - 354.80;
+                        mtbDescIRPF.Text = descIRPF.ToString("N2");
+                    }
+                    else if(SalBase > 3751.05 && SalBase <= 4664.68)
+                    {
+                        mtbAliIRPF.Text = "22,50%";
+                        descIRPF = SalBase * 0.225 - 636.13;
+                        mtbDescIRPF.Text = descIRPF.ToString("N2");
+                    }
+                    else if(SalBase > 4664.68)
+                    {
+                        mtbAliIRPF.Text = "27.50%";
+                        descIRPF = SalBase * 0.275 - 869.36;
+                        mtbDescIRPF.Text = descIRPF.ToString("N2");
+                    }
+                    else
+                    {
+                        mtbAliIRPF.Text = "0,00%";
+                        mtbDescIRPF.Text = "00,00";
+                    }
+
+                    if(SalBrut <= 1425.56)
+                    {
+                        salFami = 48.62 * NumFilhos;
+                        mtbSalFam.Text = salFami.ToString("N2");
+                    }
+                    else
+                        mtbSalFam.Text = "00,00";
+
+                    SalLiq = SalBrut - Desc - descIRPF + salFami;
+                    mtbSalLiq.Text = SalLiq.ToString("N2");
+
+                }
+                else
+                    MessageBox.Show("ERRO");
+            }
+        }
+    }
+}
